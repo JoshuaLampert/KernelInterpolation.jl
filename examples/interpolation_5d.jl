bd693e3cd9f297c46a112e251dca3eddf6748b43
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2023-12-04 16:39:00 +0100

    add example in 5D

---
+++ examples/interpolation_5d.jl
@@ -0,0 +1,16 @@
+using KernelInterpolation
+using LinearAlgebra: norm
+
+# function to interpolate
+f(x) = sum(x)
+
+n = 100
+d = 5
+nodeset = random_hypercube(n, d)
+values = f.(nodeset)
+
+kernel = WendlandKernel{d}(3, shape_parameter = 0.1)
+itp = interpolate(nodeset, values, kernel)
+
+many_nodes = homogeneous_hypercube(5, d)
+print(norm(f.(many_nodes) - itp.(many_nodes), Inf))
--- test/test_examples.jl
+++ test/test_examples.jl
@@ -41,6 +41,11 @@ EXAMPLES_DIR = examples_dir()
                               l2=0.05394435588953249, linf=0.028279879132924693)
     end
 
+    @ki_testset "interpolation_5d.jl" begin
+        @test_include_example(joinpath(EXAMPLES_DIR, "interpolation_5d.jl"),
+                              l2=0.4308925377778874, linf=0.06402624845465965)
+    end
+
     @ki_testset "interpolation_2d_convergence.jl" begin
         @test_include_example(joinpath(EXAMPLES_DIR, "interpolation_2d_convergence.jl"),
                               ns=5:10)
