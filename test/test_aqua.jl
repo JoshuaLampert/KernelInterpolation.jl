dd15597826eb5c3a2f661f25ef742b56b18243e9
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2024-03-12 19:44:27 +0100

    add test using ExplicitImports.jl (#24)

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -1,11 +1,14 @@
 module TestAqua
 
 using Aqua
+using ExplicitImports: check_no_implicit_imports, check_no_stale_explicit_imports
 using Test
 using KernelInterpolation
 
 @testset "Aqua.jl" begin
     Aqua.test_all(ambiguities = false, KernelInterpolation)
+    @test isnothing(check_no_implicit_imports(KernelInterpolation))
+    @test isnothing(check_no_stale_explicit_imports(KernelInterpolation))
 end
 
 end #module
