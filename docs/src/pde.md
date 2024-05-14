7353f96599e2d067f0bc4bcf91a17762574f6620
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2024-05-14 15:03:52 +0200

    Add general linear second-order elliptic operator (#51)
    * add general linear second-order elliptic operator and equation
    
    * format
    
    * fix test
    
    * add comma [skip ci]

--- docs/src/pde.md
+++ docs/src/pde.md
@@ -4,5 +4,5 @@ TODO:
 
 * Explain basic setup and basics of collocation for stationary and time-dependent PDEs
 * Define custom differential operators and PDEs and solve them
-* Stationary and time-dependent PDEs
+* Stationary (example Laplace in L shape) and time-dependent PDEs
 * AD vs analytic derivatives
--- src/differential_operators.jl
+++ src/differential_operators.jl
@@ -19,7 +19,7 @@ end
 """
     Gradient()
 
-The gradient operator. It can be called with a `RadialSymmetricKernel` and points
+The gradient operator. It can be called with a [`RadialSymmetricKernel`](@ref) and points
 `x` and `y` to evaluate the gradient of the `kernel` at `x - y`.
 """
 struct Gradient <: AbstractDifferentialOperator
@@ -36,7 +36,7 @@ end
 """
     Laplacian()
 
-The Laplacian operator. It can be called with a `RadialSymmetricKernel` and points
+The Laplacian operator. It can be called with a [`RadialSymmetricKernel`](@ref) and points
 `x` and `y` to evaluate the Laplacian of the `kernel` at `x - y`.
 """
 struct Laplacian <: AbstractDifferentialOperator
@@ -49,4 +49,40 @@ end
 function (::Laplacian)(kernel::RadialSymmetricKernel, x)
     H = ForwardDiff.hessian(x -> Phi(kernel, x), x)
     return tr(H)
+end
+
+@doc raw"""
+    EllipticOperator(A, b, c)
+
+Linear second-order elliptic operator with matrix ``A(x)\in\mathbb{R}^{d\times d}``, vector ``b(x)\in\mathbb{R}^d``, and scalar ``c(x)``.
+The operator is defined as
+```math
+    \mathcal{L}u = -\sum_{i,j = 1}^d a_{ij}(x)\partial_{x_i,x_j}^2u + \sum_{i = 1}^db_i(x)\partial_{x_i}u + c(x)u.
+```
+`A`, `b` and `c` are space-dependent functions returning a matrix, a vector, and a scalar, respectively. The matrix `A` should be symmetric and
+positive definite for any input `x`.
+The operator can be called with a [`RadialSymmetricKernel`](@ref) and points `x` and `y` to evaluate the operator of the `kernel` at `x - y`.
+"""
+struct EllipticOperator{AType, BType, CType} <:
+       AbstractDifferentialOperator where {AType, BType, CType}
+    A::AType
+    b::BType
+    c::CType
+end
+
+function Base.show(io::IO, ::EllipticOperator)
+    print(io, "-∑_{i,j = 1}^d aᵢⱼ (x)∂_{x_i,x_j}^2 + ∑_{i = 1}^d bᵢ(x)∂_{x_i} + c(x)")
+end
+
+function (operator::EllipticOperator)(kernel::RadialSymmetricKernel, x)
+    @unpack A, b, c = operator
+    AA = A(x)
+    bb = b(x)
+    cc = c(x)
+    H = ForwardDiff.hessian(x -> Phi(kernel, x), x)
+    gr = ForwardDiff.gradient(x -> Phi(kernel, x), x)
+
+    return sum(-AA[i, j] * H[i, j] for i in 1:dim(kernel), j in 1:dim(kernel)) +
+           sum(bb[i] * gr[i] for i in 1:dim(kernel)) +
+           cc * Phi(kernel, x)
 end
