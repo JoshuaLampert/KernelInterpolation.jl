aeb2b125359a62ac9ba6373380d2d1486e1146b6
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-12-02 11:18:56 +0100

    only one dependabot PR

--- docs/make.jl
+++ docs/make.jl
@@ -40,4 +40,6 @@ makedocs(;
 deploydocs(;
            repo = "github.com/JoshuaLampert/KernelInterpolation.jl",
            devbranch = "main",
-           push_preview = true)
+           push_preview = all(!isempty,
+                              (get(ENV, "GITHUB_TOKEN", ""),
+                               get(ENV, "DOCUMENTER_KEY", ""))))
