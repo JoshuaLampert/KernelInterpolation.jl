40474295e9774e0ae4a6fde7a3e508cf397e239b
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-03-04 17:55:50 +0100

    try reviewdog again

--- src/KernelInterpolation.jl
+++ src/KernelInterpolation.jl
@@ -28,7 +28,7 @@ using TimerOutputs: TimerOutputs, print_timer, reset_timer!
 using TrixiBase: @trixi_timeit, timer
 using TypedPolynomials: Variable, monomials, degree
 using WriteVTK: WriteVTK, vtk_grid, paraview_collection, MeshCell, VTKCellTypes,
-CollectionFile
+    CollectionFile
 
 # Define the AbstractInterpolation already here because they are needed in basis.jl
 abstract type AbstractInterpolation{Basis, Dim, RealT} end
@@ -49,7 +49,7 @@ include("util.jl")
 
 export get_name
 export GaussKernel, MultiquadricKernel, InverseMultiquadricKernel,
-       PolyharmonicSplineKernel, ThinPlateSplineKernel, WendlandKernel, WuKernel,
+PolyharmonicSplineKernel, ThinPlateSplineKernel, WendlandKernel, WuKernel,
        RadialCharacteristicKernel, MaternKernel, Matern12Kernel, Matern32Kernel,
        Matern52Kernel, Matern72Kernel, RieszKernel,
        TransformationKernel, ProductKernel, SumKernel
