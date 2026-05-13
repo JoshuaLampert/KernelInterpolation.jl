21fb9e3ca1a27d0d6428701fe0f4221a071df6f2
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-05-13 16:29:31 +0200

    add a 2D interpolation benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,12 +1,18 @@
 using BenchmarkTools
+using Random
 using KernelInterpolation
+Random.seed!(1234)
 
 const SUITE = BenchmarkGroup()
 
 f(x) = sin(sum(x))
+kernel = GaussKernel{dim(nodeset)}(shape_parameter = 0.5)
 
 nodeset = NodeSet(LinRange(0.0, 2 * pi, 8))
 values = f.(nodeset)
-kernel = GaussKernel{dim(nodeset)}(shape_parameter = 0.5)
 
 SUITE["interpolation 1D"] = @benchmarkable interpolate($nodeset, $values, $kernel)
+
+nodeset = random_hypercube(50; dim = 2)
+values = f.(nodeset)
+SUITE["interpolation 2D"] = @benchmarkable interpolate($nodeset, $values, $kernel)
