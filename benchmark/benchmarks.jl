2650ffa5011f395999a79f8cf66177e99a86beeb
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2026-05-13 16:43:28 +0200

    more benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,6 +5,7 @@ Random.seed!(1234)
 
 const SUITE = BenchmarkGroup()
 
+# Interpolation benchmarks
 f(x) = sin(sum(x))
 
 nodeset = NodeSet(LinRange(0.0, 2 * pi, 8))
@@ -18,3 +19,14 @@ values = f.(nodeset)
 kernel = ThinPlateSplineKernel{dim(nodeset)}()
 
 SUITE["interpolation 2D"] = @benchmarkable interpolate($nodeset, $values, $kernel)
+SUITE["computing Lagrange basis"] = @benchmarkable LagrangeBasis($nodeset, $kernel)
+basis = LagrangeBasis(nodeset, kernel)
+SUITE["interpolation 2D Lagrange basis"] = @benchmarkable interpolate($basis, $values)
+
+nodeset = random_hypercube(100; dim = 5)
+values = f.(nodeset)
+
+kernel = WendlandKernel{dim(nodeset)}(3, shape_parameter = 0.1)
+SUITE["interpolation 5D"] = @benchmarkable interpolate($nodeset, $values, $kernel)
+
+# PDE benchmarks
